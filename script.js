0b9ad9806619d2e9d552c14875cb62fe175a984a
ferdiboy2545 <brkkids@gmail.com>
2024-07-05 16:57:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example JavaScript to add a click event to a button
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", () => {
+    alert("Tombol diklik!");
+});
